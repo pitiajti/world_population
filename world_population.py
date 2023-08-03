c7118858ea126d8ba89ca1e368d4292bd95c99b8
pitiajti <pitiajti@gmail.com>
2023-08-03 17:37:09 +0200

    modifying countries.py and world_population.py, adding country_codes.py

--- world_population.py
+++ world_population.py
@@ -1,5 +1,7 @@
 import json
 
+from country_codes import get_country_code
+
 # Load the data into a list.
 filename = 'population_data.json'
 with open(filename) as f:
@@ -12,4 +14,9 @@ for pop_dict in pop_data:
         if pop_dict['Year'] == '2010':
             country_name = pop_dict['Country Name']
             population = int(float(pop_dict['Value']))
+            code = get_country_code(country_name)
+            if code:
+                print(f"{code}: {population}")
+            else:
+                print(f"ERROR- {country_name}")
             print(f"{country_name}: {population}")
