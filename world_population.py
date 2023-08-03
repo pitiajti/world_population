4dd30df30fa7ed694b9b785b7e0815a221b9bd12
pitiajti <pitiajti@gmail.com>
2023-08-03 17:20:51 +0200

    adding countries.py and modifying world_population.py

--- world_population.py
+++ world_population.py
@@ -11,5 +11,5 @@ for pop_dict in pop_data:
     if pop_dict in pop_data:
         if pop_dict['Year'] == '2010':
             country_name = pop_dict['Country Name']
-            population = pop_dict['Value']
+            population = int(float(pop_dict['Value']))
             print(f"{country_name}: {population}")
