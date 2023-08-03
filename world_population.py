8760c420e1a7715fa4e01fb4ecf0a8983bd11ca5
pitiajti <pitiajti@gmail.com>
2023-08-03 18:26:02 +0200

    modifying world_population.py and creating world_population.svg

--- world_population.py
+++ world_population.py
@@ -1,4 +1,5 @@
 import json
+import pygal
 
 from country_codes import get_country_code
 
@@ -6,6 +7,9 @@ from country_codes import get_country_code
 filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
+
+# Build a dictionary of population data.
+cc_populations = {}
 
 
 # Print the 2-1- population for each country.
@@ -16,7 +20,13 @@ for pop_dict in pop_data:
             population = int(float(pop_dict['Value']))
             code = get_country_code(country_name)
             if code:
-                print(f"{code}: {population}")
+                cc_populations[code] = population
             else:
                 print(f"ERROR- {country_name}")
             print(f"{country_name}: {population}")
+
+wm = pygal.maps.world.World()
+wm.title = 'World POpulation in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
